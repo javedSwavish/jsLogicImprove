3d91afa8021bfd7389b9a1a7b1f634ca2ed0206c
javedSwavish <152361768+javedSwavish@users.noreply.github.com>
2024-03-06 23:31:42 +0530

    Create jsDay1.js

---
+++ jsDay1.js
@@ -0,0 +1,23 @@
+// que1. reverse a array
+// mehtod 1
+// let arr = [1, 2, 3, 4, 5];
+// let start = 0;
+// let end = arr.length - 1;
+
+// while (start < end) {
+//   // Swap elements at start and end indices
+//   [arr[start], arr[end]] = [arr[end], arr[start]];
+
+//   // Move towards the center
+//   start++;
+//   end--;
+// }
+// console.log(arr)
+
+// method 2
+// let a=[1,2,3,4];
+// let b=[];
+// for(let i=0;i<a.length;i++){
+//   b[a.length - 1 - i]=a[i]
+// }
+// console.log(b)
